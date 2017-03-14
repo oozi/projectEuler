9e24383d5f835a24ecedb4ff49cd799f7e7aafad
Patric Holm <patric.holm@gmail.com>
2017-03-14 19:08:34 -0400

    produces correct solution

--- problem12.py
+++ problem12.py
@@ -1,60 +1,24 @@
-import math
-import math
-
-
-def get_primes(max_limit):
-    primes = [2]
-    candidate = 3
-    while candidate < max_limit:
-        is_prime = True
-        for p in primes:
-            if candidate % p == 0:
-                is_prime = False
-        if is_prime:
-            primes.append(candidate)
-        candidate += 1
-    return primes
-
-
-def get_divisors(x, primes = []):
-    divisors = [1, x]
-
-    if not primes:
-        primes = get_primes(x)
-
-    for p in primes:
-        if p > math.sqrt(x):
-            break
-        p_temp = p
-        while x % p_temp == 0:
-            if p_temp not in divisors:
-                divisors.append(p_temp)
-                mirror = x/p_temp
-                divisors.append(mirror)
-            p_temp += p
-    divisors.sort()
-    return divisors
+from problem12lib import *
 
 
 if __name__ == '__main__':
-    primes = get_primes(4000)
+    primes = get_primes(500)
     i = 40
     s = i * (i + 1) / 2
-    s = 17907120
     divs = get_divisors(s, primes)
     max_divs = len(divs)
-    while len(divs) < 100:
+    while len(divs) < 500:
         i += 1
         if i % 1000 == 0:
-            print i
+            print(i)
         s = i * (i + 1) / 2
         divs = get_divisors(s, primes)
         if len(divs) > max_divs:
             max_divs = len(divs)
-            print "maxdivs: ", max_divs
-    print divs
-    print "i: ", i
-    print "s: ", s
-    print "d: ", len(divs)
+            print("max divs: ", max_divs)
+    print(divs)
+    print("i: ", i)
+    print("s: ", s)
+    print("d: ", len(divs))
 
 
--- problem12test.py
+++ problem12test.py
@@ -1,1 +1,31 @@
-import problem12
+import unittest
+from problem12lib import *
+
+
+class GetPrimesTest(unittest.TestCase):
+    def test_input(self):
+        self.assertRaises(ValueError, get_primes, -1)
+        self.assertRaises(TypeError, get_primes, 9.6)
+
+    def test_one(self):
+        self.assertEqual(get_primes(1), [])
+
+    def test_primes(self):
+        self.assertEqual(get_primes(20), [2, 3, 5, 7, 11, 13, 17, 19])
+
+
+class GetDivisorTest(unittest.TestCase):
+    def test_input(self):
+        self.assertRaises(ValueError, get_divisors, -1)
+        self.assertRaises(ValueError, get_divisors, 0)
+        self.assertRaises(TypeError, get_divisors, 9.6)
+
+    def test_one(self):
+        self.assertEqual(get_divisors(1), [1])
+
+    def test_divisors(self):
+        self.assertEqual(get_divisors(200), [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 200])
+
+
+if __name__ == '__main__':
+    unittest.main()
